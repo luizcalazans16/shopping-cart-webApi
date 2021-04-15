0d7246c60cc3880d3a037a2485c80b34e168ce16
Luiz Calazans <luiz.calazans@viaflow.com.br>
2021-04-15 10:04:23 -0300

    Update exception handler

--- shopping-cart-app/business-core-app/src/main/java/br/com/calazans/shoppingcart/app/core/exceptions/Problem.java
+++ shopping-cart-app/business-core-app/src/main/java/br/com/calazans/shoppingcart/app/core/exceptions/Problem.java
@@ -1,32 +1,34 @@
 package br.com.calazans.shoppingcart.app.core.exceptions;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 public class Problem {
 
+    private String requestUri;
+    private String name;
     private Integer status;
     private OffsetDateTime dateTime;
-    private String title;
     private String message;
-    private List<Field> fields;
+    private List<Integer> statusCodes = new ArrayList<>();
 
-    public Problem() {
+    public Problem(String message, HttpServletRequest request, HttpStatus httpStatus) {
+        setMessage(message);
+        setName(httpStatus.getReasonPhrase());
+        setRequestUri(request.getRequestURI());
         setDateTime(OffsetDateTime.now());
-    }
-
-    public Problem(String errorMessage) {
-        setMessage(errorMessage);
-    }
-
-    @Data
-    public static class Field {
-        private String name;
-        private String message;
+        statusCodes.add(httpStatus.value());
+        setStatusCodes(statusCodes);
     }
 }
--- shopping-cart-app/business-core-app/src/main/java/br/com/calazans/shoppingcart/app/core/exceptions/handler/ApiExceptionHandler.java
+++ shopping-cart-app/business-core-app/src/main/java/br/com/calazans/shoppingcart/app/core/exceptions/handler/ApiExceptionHandler.java
@@ -12,21 +12,25 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<Object> handleBusiness(BusinessException ex, WebRequest request) {
+    public ResponseEntity<Object> handleBusiness(HttpServletRequest request, BusinessException ex) {
         var status = HttpStatus.BAD_REQUEST;
-        var problem = new Problem(ex.getMessage());
+        var problem = new Problem(ex.getMessage(), request, status);
 
         return handleExceptionInternal(ex, problem, new HttpHeaders(), status, null);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+    public ResponseEntity<Object> handleResourceNotFoundException(HttpServletRequest request,
+                                                                  ResourceNotFoundException ex) {
         var status = HttpStatus.BAD_REQUEST;
-        var problem = new Problem(ex.getMessage());
+        var problem = new Problem(ex.getMessage(), request, status);
 
         return handleExceptionInternal(ex, problem, new HttpHeaders(), status, null);
     }
